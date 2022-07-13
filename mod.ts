da10b9dfe302350129441c3480ca50047a57aa2a
Slim Hmidi <31925861+slim-hmidi@users.noreply.github.com>
2022-07-13 14:51:17 +0200

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,20 +1,15 @@
-export * as ConsoleColor from "https://deno.land/x/colorlog@v1.0/mod.ts";
-
-export { default as SQLQueryBuilder } from "https://raw.githubusercontent.com/Zhomart/dex/update-deps/mod.ts";
-
-export { camelCase, snakeCase } from "https://deno.land/x/case@2.1.1/mod.ts";
-
-export {
-  Client as MySQLClient,
-  configLogger as configMySQLLogger,
-  Connection as MySQLConnection,
-} from "https://deno.land/x/mysql@v2.10.2/mod.ts";
-export type { LoggerConfig } from "https://deno.land/x/mysql@v2.10.2/mod.ts";
-
-export { Client as PostgresClient } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
-
-export { DB as SQLiteClient } from "https://deno.land/x/sqlite@v3.4.0/mod.ts";
-
-export { MongoClient as MongoDBClient, Bson } from "https://deno.land/x/mongo@v0.30.1/mod.ts";
-export type { ConnectOptions as MongoDBClientOptions } from "https://deno.land/x/mongo@v0.30.1/mod.ts";
-export type { Database as MongoDBDatabase } from "https://deno.land/x/mongo@v0.30.1/src/database.ts";
+export { DATA_TYPES, DataTypes } from "./lib/data-types.ts";
+export { Database } from "./lib/database.ts";
+export type { DatabaseOptions, SyncOptions } from "./lib/database.ts";
+export { Model } from "./lib/model.ts";
+export { Relationships } from "./lib/relationships.ts";
+export type { Connector } from "./lib/connectors/connector.ts";
+export { MongoDBConnector } from "./lib/connectors/mongodb-connector.ts";
+export type { MongoDBOptions } from "./lib/connectors/mongodb-connector.ts";
+export { MySQLConnector } from "./lib/connectors/mysql-connector.ts";
+export type { MySQLOptions } from "./lib/connectors/mysql-connector.ts";
+export { PostgresConnector } from "./lib/connectors/postgres-connector.ts";
+export type { PostgresOptions } from "./lib/connectors/postgres-connector.ts";
+export { SQLite3Connector } from "./lib/connectors/sqlite3-connector.ts";
+export type { SQLite3Options } from "./lib/connectors/sqlite3-connector.ts";
+export { connectorFactory } from "./lib/connectors/factory.ts";
