e5bc2b4e626a17b583da52babb5df6b54c3c0c9f
Slim Hmidi <31925861+slim-hmidi@users.noreply.github.com>
2022-07-13 14:58:18 +0200

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,22 +1,20 @@
+export { default as SQLQueryBuilder } from "https://raw.githubusercontent.com/Zhomart/dex/update-deps/mod.ts";
 export * as ConsoleColor from "https://deno.land/x/colorlog@v1.0/mod.ts";
 
-// NOTE: Migrate to the official https://github.com/aghussb/dex when it's updated to the
-//       latest deno version.
-export { default as SQLQueryBuilder } from "https://raw.githubusercontent.com/Zhomart/dex/update-deps/mod.ts";
-
-export { camelCase, snakeCase } from "https://deno.land/x/case@2.1.1/mod.ts";
+export { camelCase, snakeCase } from "https://deno.land/x/case@v2.1.0/mod.ts";
 
 export {
   Client as MySQLClient,
   configLogger as configMySQLLogger,
   Connection as MySQLConnection,
-} from "https://deno.land/x/mysql@v2.10.2/mod.ts";
-export type { LoggerConfig } from "https://deno.land/x/mysql@v2.10.2/mod.ts";
+} from "https://deno.land/x/mysql@v2.10.1/mod.ts";
+export type { LoggerConfig } from "https://deno.land/x/mysql@v2.10.1/mod.ts";
 
-export { Client as PostgresClient } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
+export { Client as PostgresClient } from "https://deno.land/x/postgres@v0.14.2/mod.ts";
 
-export { DB as SQLiteClient } from "https://deno.land/x/sqlite@v3.4.0/mod.ts";
+export { DB as SQLiteClient } from "https://deno.land/x/sqlite@v3.1.3/mod.ts";
 
-export { MongoClient as MongoDBClient, Bson } from "https://deno.land/x/mongo@v0.30.1/mod.ts";
-export type { ConnectOptions as MongoDBClientOptions } from "https://deno.land/x/mongo@v0.30.1/mod.ts";
-export type { Database as MongoDBDatabase } from "https://deno.land/x/mongo@v0.30.1/src/database.ts";
+export { MongoClient as MongoDBClient, Bson } from "https://deno.land/x/mongo@v0.28.1/mod.ts";
+export type { ConnectOptions as MongoDBClientOptions } from "https://deno.land/x/mongo@v0.28.1/mod.ts";
+export type { Database as MongoDBDatabase } from "https://deno.land/x/mongo@v0.28.1/src/database.ts";
+
